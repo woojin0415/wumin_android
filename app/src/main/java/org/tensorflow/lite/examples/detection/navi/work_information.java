dab78afeb52cab1fb5eb875c52e2d4036fe830a4
woojin0415 <75172426+woojin0415@users.noreply.github.com>
2023-12-02 02:14:10 +0900

    2023 12 02 woojin - navigation guide ment edit

--- app/src/main/java/org/tensorflow/lite/examples/detection/navi/work_information.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/navi/work_information.java
@@ -9,9 +9,10 @@ public class work_information {
         informations = new String[section_size][numofwork][3];
     }
 
-    void set_work(int section, int number, String name, String desc){
+    void set_work(int section, int number, String name, String artist, String desc){
         informations[section][number][0] = name;
-        informations[section][number][1] = desc;
+        informations[section][number][1] = artist;
+        informations[section][number][2] = desc;
     }
 
     String [] get_work(int section, int number){
